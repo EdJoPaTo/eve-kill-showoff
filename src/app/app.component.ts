84ae4942cbfb33dda5a49a8a48df4abc8f57335d
Edgar Toll <edjopato@gmail.com>
2016-11-22 08:39:33 +0100

    load the killmails and display it with the kill-list
    instead of let the list load it itself

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { KillmailService, Killmail } from './z-killboard';
+import { LoadKillsService } from './load-kills.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private killmails: Killmail[] = [];
 
+  constructor(
+    private killmailService: KillmailService,
+    private loadKillsService: LoadKillsService
+  ) { }
+
+  ngOnInit() {
+    this.loadKillsService.get()
+      .flatMap(ids => ids)
+      .flatMap(id => this.killmailService.get(id))
+      .reduce((sum, add) => sum.concat(add), [])
+      .subscribe(killmails => this.killmails = killmails);
+  }
 }
--- src/app/kill-list/kill-list.component.ts
+++ src/app/kill-list/kill-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-import { KillmailService, Killmail } from '../z-killboard';
-import { LoadKillsService } from '../load-kills.service';
+import { Killmail } from '../z-killboard';
 
 @Component({
   selector: 'app-kill-list',
@@ -9,19 +8,12 @@ import { LoadKillsService } from '../load-kills.service';
   styleUrls: ['./kill-list.component.scss']
 })
 export class KillListComponent implements OnInit {
-  private killmails: Killmail[] = [];
+  @Input() killmails: Killmail[];
 
   constructor(
-    private killmailService: KillmailService,
-    private loadKillsService: LoadKillsService
   ) { }
 
   ngOnInit() {
-    this.loadKillsService.get()
-      .flatMap(ids => ids)
-      .flatMap(id => this.killmailService.get(id))
-      .reduce((sum, add) => sum.concat(add), [])
-      .subscribe(killmails => this.killmails = killmails);
   }
 
 }
