7863f073b7df8269481f97e800140168f08121b0
Edgar Toll <edjopato@gmail.com>
2016-11-22 23:19:59 +0100

    order kills by date (newest first)

--- src/app/kill-view/kill-view.component.ts
+++ src/app/kill-view/kill-view.component.ts
@@ -41,6 +41,7 @@ export class KillViewComponent implements OnInit {
       .flatMap(ids => ids)
       .flatMap(id => this.killmailService.get(id))
       .reduce((sum, add) => sum.concat(add), [])
+      .map(list => { list.sort((a, b) => new Date(b.killTime) - new Date(a.killTime)); return list; })
       .share();
 
     this.killmails
