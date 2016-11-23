6d9379254daa06b80969a7cd9852baaaec939137
Edgar Toll <edjopato@gmail.com>
2016-11-23 01:52:18 +0100

    ensure numbers are numbers (or parse strings to numbers)

--- src/app/shared/kill-time-to-date.pipe.ts
+++ src/app/shared/kill-time-to-date.pipe.ts
@@ -7,7 +7,14 @@ export class KillTimeToDatePipe implements PipeTransform {
 
   static toDate(killTime: string): Date {
     let splitted = killTime.split(/-| |:/);
-    let date = Date.UTC(splitted[0], splitted[1] - 1, splitted[2], splitted[3], splitted[4], splitted[5]);
+    let date = Date.UTC(
+      Number(splitted[0]),
+      Number(splitted[1]) - 1,
+      Number(splitted[2]),
+      Number(splitted[3]),
+      Number(splitted[4]),
+      Number(splitted[5])
+    );
     return new Date(date);
   }
 
