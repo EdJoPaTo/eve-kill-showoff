f7cdfed1ba83942124b811efdd7c3ff6b06502cd
Edgar Toll <edjopato@gmail.com>
2016-12-11 11:55:28 +0100

    Add total damage, killamount, involved pilots, ISK destroyed (per Month)

--- src/app/kill-view/killmail-view/killmail-view.component.ts
+++ src/app/kill-view/killmail-view/killmail-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, DoCheck, Input } from '@angular/core';
 
 import { Killmail } from '../../z-killboard';
 
@@ -7,12 +7,34 @@ import { Killmail } from '../../z-killboard';
   templateUrl: './killmail-view.component.html',
   styleUrls: ['./killmail-view.component.scss']
 })
-export class KillmailViewComponent implements OnInit {
+export class KillmailViewComponent implements OnInit, DoCheck {
   @Input() killmails: Killmail[];
+  damageDealt: number = 0;
+  pilotsInvolved: number = 0;
+  iskDestroyed: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngDoCheck() {
+    if (this.killmails) {
+      this.damageDealt = this.killmails
+        .map(k => k.victim.damageTaken)
+        .reduce((a, b) => a + b, 0);
+
+      this.pilotsInvolved = this.killmails
+        .map(k => k.attackers)
+        .reduce((a, b) => a.concat(b), []) // merge all into a single array
+        .map(attacker => attacker.characterID)
+        .filter((v, i, a) => a.indexOf(v) === i) // only unique
+        .length;
+
+      this.iskDestroyed = this.killmails
+        .map(k => k.zkb.totalValue)
+        .reduce((a, b) => a + b, 0);
+    }
   }
 
 }
